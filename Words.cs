73ef01b9832dd483af6354f7aaafa0e9a5148382
lindzr <munnsli@byui.edu>
2022-10-18 15:19:58 -0600

    wrote a small program in jumper.cs to check if my class is working

--- Words.cs
+++ Words.cs
@@ -9,16 +9,22 @@ class Words
         if (difficulty == "easy")
         {
             List <string> wordbank = new List<string>(){"reach","heart","tiger","football"};
+            int index = Random.Next(wordbank.Count);
+             return Console.WriteLine(wordbank[index]);
         }
     
         else if (difficulty == "intermediate")
         {
             List <string> wordbank = new List<string>(){"blizzard", "staff", "kiwi", "witch", "zipper"};
+            int index = Random.Next(wordbank.Count);
+            return Console.WriteLine(wordbank[index]);
         }
 
         else
         {
             List <string> wordbank = new List<string>(){"waltz", "numbskull", "pneumonia", "marquis", "kitsch", "jazz"};
+            int index = Random.Next(wordbank.Count);
+            return Console.WriteLine(wordbank[index]);
         }
 
     }
