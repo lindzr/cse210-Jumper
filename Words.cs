3fa1e93449218140ce93a823bafef145c129a06e
lindzr <munnsli@byui.edu>
2022-10-18 14:51:04 -0600

    Create Words.cs

---
+++ Words.cs
@@ -0,0 +1,25 @@
+// Ask for difficulty and picks random word from wordbanks
+
+class Words
+{
+    string difficulty = "";
+
+    public string retrieveWord(string difficulty)
+    {
+        if (difficulty == "easy")
+        {
+            List <string> wordbank = new List<string>(){"reach","heart","tiger","football"};
+        }
+    
+        else if (difficulty == "intermediate")
+        {
+            List <string> wordbank = new List<string>(){"blizzard", "staff", "kiwi", "witch", "zipper"};
+        }
+
+        else
+        {
+            List <string> wordbank = new List<string>(){"waltz", "numbskull", "pneumonia", "marquis", "kitsch", "jazz"};
+        }
+
+    }
+}
