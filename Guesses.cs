44b30cc2e7367736eaa567c8280422ebad96bb8c
elenal63 <91587602+elenal63@users.noreply.github.com>
2022-10-22 14:14:10 -0600

    update

--- Guesses.cs
+++ Guesses.cs
@@ -7,21 +7,42 @@
 class Guesses{
     List<string> previousGuesses = new List<string>;
     //insert getter from Words class to retrieve the word and letter/guess
-    public string word = GetWord();
+    public string word = retrieveWord(difficulty);
     public string letter = GetLetter();
     public int numLetters = 0; 
 
-    public bool GuessIsCorrect()
+//this function finds the number of occurences each letter has in the word. If a letter occurs multiple times numLettter will count those occurences and get returned.
+    public int numRepeats()
     {
         do
         {
             numLetters = numLetters + 1;
-            previousGuesses.Add(letter);
+            
         } 
-        while (word.contains(letter));
+        while (word.Contains(letter));
 
         return numLetters;
     }
+
+//this function determines if the users guess was in the letter or not by returning a boolean
+    public bool guessIsCorrect()
+        {
+            do
+            {
+                bool correctGuess = true;
+            } 
+            while (word.Contains(letter));
+
+            return correctGuess;
+        }
+
+    public List<string> guessesBank(string letter)
+        {
+            previousGuesses.Add(letter);
+            return previousGuesses;
+        }
+
+
 
     //make a method to return a bool if guess is correct
     //make a method to return the number of instances the letter appears in the word
